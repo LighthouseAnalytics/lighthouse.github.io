01d5a24f5156ac47ad3baf5706369b92d4bf4e8e
sganeriwal <sganeriwal@paypal.com>
2018-06-20 14:13:48 +0530

    more time

--- app/quizCtrl.js
+++ app/quizCtrl.js
@@ -15,7 +15,7 @@
         'allowBack': true,
         'allowReview': true,
         'autoMove': false,  // if true, it will move to next question automatically when answered.
-        'duration': 0,  // indicates the time in which quiz needs to be completed. post that, quiz will be automatically submitted. 0 means unlimited.
+        'duration': 120,  // indicates the time in which quiz needs to be completed. post that, quiz will be automatically submitted. 0 means unlimited.
         'pageSize': 1,
         'requiredAll': false,  // indicates if you must answer all the questions before submitting.
         'richText': false,
